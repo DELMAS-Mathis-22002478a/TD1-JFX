fa99db31879c32d275373dee80eca2be8b8ba9ca
d22002478 <mathis.delmas@etu.univ-amu.fr>
2023-05-15 11:28:25 +0200

    partie 3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,18 @@ module com.example.td1jfx {
     requires javafx.fxml;
 
 
+
     opens com.example.td1jfx to javafx.fxml;
     exports com.example.td1jfx;
     exports com.example.partie1;
     exports com.example.partie2;
     exports exercice1;
+    exports com.example.javafxdemo.partie2.exercice5;
+    opens com.example.partie3 to javafx.fxml;
+    exports com.example.partie3;
+
+
+
 
 
 }
